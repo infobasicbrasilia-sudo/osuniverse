365c9a2969d9ed21d7ef8bccfab7c25b2937e69b
infobasicbrasilia-sudo <infobasicbrasilia@gmail.com>
2026-04-26 15:41:52 -0300

    Word: corrige régua (texto move ao arrastar), adiciona aba Editar com Desfazer/Refazer

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,12 @@
-function ligar(){
+function ligar() {
+    // Adiciona efeito de clique no botão
+    const btn = document.querySelector('.power-btn');
+    btn.style.opacity = '0.5';
+    btn.disabled = true;
+    btn.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Iniciando...';
 
-window.location.href = "/bios"
-
+    // Simula o POST (ligar o computador) e redireciona para a BIOS
+    setTimeout(() => {
+        window.location.href = "/bios";
+    }, 800);
 }
